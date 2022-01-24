6c53ffce2bacaf5bad75e4dee0f022e47f1265b7
Sanjot Singh <59414089+sanjotsingh99@users.noreply.github.com>
2022-01-24 16:31:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import pickle
+import streamlit as st
+import plotly.graph_objects as go
+#import pandas as pd
+st.header("PRODUCTS")
+df = pickle.load(open('df.pkl','rb'))
+
+trace = go.Table(
+    header=dict(values=list(df[['Title', 'Price','Popularity']].columns),
+                fill_color='paleturquoise',
+                align='left',font=dict(color='black', size=15),
+    height=40),
+    cells=dict(values=[df.Title, df.Price, df.Popularity],
+               fill_color='lavender',
+               align='left',font_size=15,
+    height=30))
+
+fig=go.Figure(data=[trace])
+fig.update_layout(margin=dict(l=20, r=20, t=20, b=20),paper_bgcolor="white")
+st.write(fig)
